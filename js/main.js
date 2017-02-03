e242375e151626afe42f993ecbbb20095e65b350
Maricela Alvarez <malvarezdrobush@gmail.com>
2017-02-03 15:23:04 -0500

    Agregando tip

--- js/main.js
+++ js/main.js
@@ -24,19 +24,24 @@ function drawTasksList(){
     lista.innerHTML="";
     
     for(var i in listaTareas)
-    {
+    {   
+
         var html = "<li class='contenedorTarea'"+(listaTareas[i].isDone?"class='tachado'":"")+"><input onclick='onCheckClick("+i+")' type='checkbox' " + (listaTareas[i].isDone?"checked":"") + "> " +(listaTareas[i].nombre)+"<i onclick='onTrashClick("+i+")' class='glyphicon glyphicon-trash tacho'></i></li>";
+        
+        if(listaTareas.length>0){
+            lista
+        }
         
         //listaTareas[i].classList.add="option animated rotateInDownLeft";
         lista.innerHTML += html;
         
-    }
-    
+    }    
 }
 
 function onCheckClick(input)
 {
-    
+    /*listaTareas[input].isDone = !listaTareas[input].isDone;
+    drawTasksList();*/
     if(listaTareas[input].isDone==false){
         listaTareas[input].isDone=true;
         lista.getElementsByClassName("contenedorTarea")[input].classList.add("tachado");
